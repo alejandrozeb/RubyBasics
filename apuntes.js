0bd616c619af0f1a63022a5bf5f4a1d3cd2d0131
unknown <alejandrojzeballos@gmail.com>
2021-12-25 11:28:13 -0400

    {add} ventajas y desventajas de ruby

--- apuntes.js
+++ apuntes.js
@@ -1,6 +1,29 @@
 /* 
 inicio curso ruby
 
+Matz en 1995
+opensource
+2005 framework
+dinamico, cambiar de tipos
+interpretado
+    lee el archivo y se lee linea por linea
+    jruby(java), cruby, mruby(binarios)
+Orientado a objetos
+"felicidad y productividad del desarrollador"
+
+ventajas
+comunidad
+muchas librerias
+lenguaje maduro
+metaprogramacion: capaciad de modificar su estructura
+DSls minilenguaje de programacion, background
+
+desventajas
+interpretado, lento
+alto uso de memoria
+no paralelismo (con cruby un core a la vez)
+no tiene una compania grande
+ha venido decreciendo en popularidad
 
 
 

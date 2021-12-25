312c7a60ac0e4371aeb63c5ad2bcf2466cbaa6a0
unknown <alejandrojzeballos@gmail.com>
2021-12-25 11:58:58 -0400

    {add} integers y floats

---
+++ tipos de datos/integer.rb
@@ -0,0 +1,18 @@
+x = 5 =begin todo es un objeto todo tiene metodos =end
+x.class  =begin devuelve el tipo de clase =end
+x.methos =begin devuelve todos los datos =end
+x.odd? =begin impar =end
+x.even? =begin ? son parte del metodo, retorna un true o false =end
+
+x+5
+x-6
+x/7
+x*4
+x%3
+x**2 =begin potencia =end
+
+f=2.4 =begin float  =end
+4/5 =begin 0 convierte de entero a entero =end
+4/5.0 =begin convierte a tipo decimal =end
+edad_cliente =begin convencion de escritura de variables =end
+x=100**100 =begin ruby permite usar numeros grandes sin necesidad de usar liberiras =end
